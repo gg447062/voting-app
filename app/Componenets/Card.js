a8f2f05be2e0bc12a4735cf14ca7b4036d560c48
Gabriel Gutierrez <gg447062@gmail.com>
2022-01-19 15:53:16 -0600

    finishes basic layout of card and footer components

--- app/Componenets/Card.js
+++ app/Componenets/Card.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player';
 import { getRecords } from '../Airtable';
 
 const Card = ({ current }) => {
   const [allApps, setAllApps] = useState(null);
+  const currentProject = allApps ? allApps[current] : null;
 
   useEffect(() => {
     getRecords(setAllApps);
@@ -12,15 +14,31 @@ const Card = ({ current }) => {
     <div>
       {allApps && (
         <div>
-          <h1>{allApps[current]['Project Name']}</h1>
-          <p>{allApps[current]['Contact Name']}</p>
-          <iframe
-            // src={allApps[current]['Video']}
-            src="https://drive.google.com/file/d/1RRxzPtHiUzg8xQoqPgOPNLipvYcZ1gtz/preview"
-            controls
-            // crossOrigin="use-credentials"
-          ></iframe>
-          <div>{allApps[current]['Call to Adventure']}</div>
+          <h1>{currentProject['Project Name']}</h1>
+          <p>{currentProject['Contact Name']}</p>
+          <ReactPlayer url={currentProject['Video']} controls={true} />
+          <div>
+            <h2>Call to Adventure</h2>
+            {currentProject['Call to Adventure']}
+          </div>
+          <div>
+            <h3>Reviewer Scores</h3>
+            <p>85 - Josh</p>
+            <p>
+              This is a great project that is awesome and should be a part of
+              the accelerator because it’s GMI.
+            </p>
+            <p>75 - John</p>
+            <p>
+              This is a great project that is awesome and should be a part of
+              the accelerator because it’s GMI.
+            </p>
+            <h3>Curated Lists</h3>
+            <p>Appears on the curated lists of:</p>
+            <p>Malcolm</p>
+            <p>Jonathan</p>
+            <p>Raf</p>
+          </div>
         </div>
       )}
     </div>
--- app/Componenets/Footer.js
+++ app/Componenets/Footer.js
@@ -3,13 +3,25 @@ import React from 'react';
 const Footer = ({ current, setCurrent }) => {
   return (
     <div>
+      <p>{current} of 32 projects reviewed</p>
       <button
         onClick={() => {
           setCurrent(current + 1);
         }}
       >
-        NEXT
+        Thumbs Up
       </button>
+      <button
+        onClick={() => {
+          setCurrent(current + 1);
+        }}
+      >
+        Thumbs Down
+      </button>
+      <div>
+        <p>Voting power: XXX $CLUB</p>
+        <p>You'll be able to allocate votes after your initial review</p>
+      </div>
     </div>
   );
 };
