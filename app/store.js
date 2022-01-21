fece22e654c814194f5c14e124057510886e1772
Gabriel Gutierrez <gg447062@gmail.com>
2022-01-21 14:32:25 -0600

    adds redux logging middleware

--- app/store.js
+++ app/store.js
@@ -1,5 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import appReducer from './Redux';
 
-export default createStore(appReducer, applyMiddleware(thunk));
+const middleware = [thunk, createLogger({ collapsed: true })];
+
+export default createStore(
+  appReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
