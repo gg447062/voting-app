6c14af37bda57f0e688563196f16b2caf000918f
Gabriel Gutierrez <gg447062@gmail.com>
2022-02-07 13:13:26 -0600

    adds results component

--- app/Components/App.js
+++ app/Components/App.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import Vote from './Vote';
 import Review from './Review';
 import Connect from './Connect';
+import Results from './Results';
 
 const App = () => {
   const complete = useSelector((state) => state.applications.complete);
@@ -12,9 +13,10 @@ const App = () => {
       {/* <main className="is-family-monospace"> */}
       <Router>
         <Routes>
-          {/* <Route path={'/'} element={<Connect />} /> */}
-          <Route path={'/'} element={<Review />} />
+          <Route path={'/'} element={<Connect />} />
+          <Route path={'review'} element={<Review />} />
           <Route path={'vote'} element={<Vote />} />
+          <Route path={'results'} element={<Results />} />
         </Routes>
       </Router>
     </main>
---
+++ app/Components/Results.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Results = () => {
+  return (
+    <div className="is-flex-column has-border has-shadow">
+      <p style={{ textAlign: 'center', margin: '10px' }}>thanks for voting</p>
+    </div>
+  );
+};
+
+export default Results;
