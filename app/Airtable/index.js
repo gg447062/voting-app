9e1879fbcda85322003766c15944dfea4a4fd3ca
Gabriel Gutierrez <gg447062@gmail.com>
2022-02-07 13:11:33 -0600

    updates api calls

--- app/Airtable/index.js
+++ app/Airtable/index.js
@@ -1,6 +1,56 @@
-const Airtable = require('airtable');
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  'appZy0IcocUjMrryI'
-);
+import axios from 'axios';
 
-export default base;
+axios.defaults.headers[
+  'Authorization'
+] = `Bearer ${process.env.AIRTABLE_API_KEY}`;
+
+const applicationsBaseURL =
+  'https://api.airtable.com/v0/appZy0IcocUjMrryI/SC04%20Applications';
+
+const votesBaseURL =
+  'https://api.airtable.com/v0/appZy0IcocUjMrryI/Votes%20Test';
+
+export const getRecords = async () => {
+  const { data: res } = await axios.get(applicationsBaseURL, {
+    params: {
+      view: 'Final List',
+      fields: ['Project Name', 'Contact Name', 'Video', 'Call to Adventure'],
+    },
+  });
+
+  const fields = res.records.map((record) => {
+    return record.fields;
+  });
+
+  return fields;
+};
+
+export const getVoter = async (id) => {
+  const { data: res } = await axios.get(votesBaseURL, {
+    params: {
+      view: 'Main',
+      fields: ['Voter ID', 'Voted'],
+    },
+  });
+
+  const voter = res.records.filter(
+    (record) => record.fields['Voter ID'].toLowerCase() == id.toLowerCase()
+  )[0];
+
+  return voter;
+};
+
+export const vote = async (id, votes) => {
+  try {
+    const { data } = await axios.patch(`${votesBaseURL}/${id}`, {
+      fields: {
+        Voted: 'yes',
+        Votes: votes,
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
