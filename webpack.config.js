c98211ae874f475d30f436188f7c2ff4cafd03a8
Gabriel Gutierrez <gg447062@gmail.com>
2022-02-09 13:29:50 -0600

    updates webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -28,5 +28,8 @@ module.exports = {
       },
     ],
   },
-  plugins: [new NodePolyfillPlugin(), new Dotenv()],
+  plugins: [
+    new NodePolyfillPlugin(),
+    new Dotenv({ path: path.resolve(__dirname, '.env'), systemvars: true }),
+  ],
 };
