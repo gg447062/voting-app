6028799545f978dfc4869ee6c36f05c8bfa3ee35
Gabriel Gutierrez <gg447062@gmail.com>
2022-05-09 20:18:18 -0500

    updates overview component

--- app/Components/Overview/index.js
+++ app/Components/Overview/index.js
@@ -9,26 +9,27 @@ import { fetchRecords, setCurrent } from '../../Redux/applications';
 import Modal from '../Modal';
 
 const Review = () => {
-  const address = useSelector((state) => state.account.address);
-  const name = useSelector((state) => state.account.name);
-  const [display, setDisplay] = useState('block');
+  const all = useSelector((state) => state.applications.all);
+  const [display, setDisplay] = useState('hidden');
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchRecords());
-  });
+  }, []);
 
   return (
     <React.Fragment>
       <div
         id="review"
-        className="overview flex center column"
+        className="overview flex center column blue-gradient"
         // style={{ filter: 'blur(5px)' }}
       >
-        <Header align="center">
-          <div className="address ff-serif">{name ? name : address}</div>
-          <ConnectButton />
-        </Header>
+        <Header
+          align="sb"
+          title="Projects Reviewed"
+          val1={0}
+          val2={all.length}
+        />
         <ProjectGrid setDisplay={setDisplay} />
         <Top10></Top10>
         <Modal display={display} setDisplay={setDisplay}>
